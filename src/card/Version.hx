c2fc43b79085df78a6256607b6548d69d9a098c2
Cédric Belin <cedric@belin.io>
2021-12-18 15:21:52 +0100

    Added the `Version` class

---
+++ src/card/Version.hx
@@ -0,0 +1,36 @@
+package card;
+
+#if macro
+import haxe.Json;
+import haxe.macro.Context;
+import sys.io.File;
+import sys.io.Process;
+#end
+
+/** Provides information about the program version. **/
+abstract class Version {
+
+	/** Gets the hash of the current Git commit. **/
+	macro public static function getGitCommitHash() {
+		#if display
+			return macro $v{""};
+		#else
+			final process = new Process("git", ["rev-parse", "HEAD"]);
+			final hash = process.exitCode() == 0 ? process.stdout.readLine() : process.stderr.readLine();
+			process.close();
+			return macro $v{hash};
+		#end
+	}
+
+	/** Gets the name of the Haxe target. **/
+	macro public static function getHaxeTarget()
+		return macro $v{Context.definedValue("target.name")};
+
+	/** Gets the version of the Haxe compiler. **/
+	macro public static function getHaxeVersion()
+		return macro $v{Context.definedValue("haxe")};
+
+	/** Gets the package version of this program. **/
+	macro public static function getPackageVersion()
+		return macro $v{#if display "0.0.0" #else Json.parse(File.getContent("haxelib.json")).version #end};
+}
---
+++ src/card/import.hx
@@ -0,0 +1,4 @@
+import tink.Cli;
+import tink.cli.Rest;
+
+using tink.CoreApi;
