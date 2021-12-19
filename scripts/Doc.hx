9640de602003c8cb420149cb1ae6fe75576c0ac3
Cédric Belin <cedric@belin.io>
2021-12-19 14:24:04 +0100

    Updated the build system

---
+++ scripts/Doc.hx
@@ -0,0 +1,26 @@
+import Sys.*;
+import Tools.removeDirectory;
+import haxe.Json;
+import sys.FileSystem.*;
+import sys.io.File.*;
+
+/** Runs the script. **/
+function main() {
+	if (exists("docs")) removeDirectory("docs");
+
+	command("haxe --define doc-gen --no-output --xml var/api.xml build.hxml");
+	command("lix", [
+		"run", "dox",
+		"--define", "description", "Find the instances of an executable in the system path, in Haxe.",
+		"--define", "source-path", "https://bitbucket.org/cedx/which.hx/src/main/src",
+		"--define", "themeColor", "0xffc105",
+		"--define", "version", Json.parse(getContent("haxelib.json")).version,
+		"--define", "website", "https://bitbucket.org/cedx/which.hx",
+		"--input-path", "var",
+		"--output-path", "docs",
+		"--title", "Which for Haxe",
+		"--toplevel-package", "which"
+	]);
+
+	copy("www/favicon.ico", "docs/favicon.ico");
+}
