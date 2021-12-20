b3edc3e7607c47f8c8657e8c67f8b944eb186e7d
Cédric Belin <cedric@belin.io>
2021-12-20 21:07:20 +0100

    Updated the build system

--- scripts/Dist.hx
+++ scripts/Dist.hx
@@ -3,17 +3,12 @@ import sys.io.File.*;
 
 /** Runs the script. **/
 function main() {
-	final isWindows = systemName() == "Windows";
-	for (script in ["Clean", "Build"]) command('lix $script');
+	for (script in ["Clean", "Version"]) command('lix $script');
+	command("haxe --dce full build.hxml");
 
-	var file = "bin/cedx";
-	saveContent(file, '#!/usr/bin/env php\n${getContent(file)}');
-	command('git update-index --chmod=+x $file');
-	if (!isWindows) command('chmod +x $file');
-
-	file = "bin/cedx.js";
+	final file = "bin/card.js";
 	command('npx terser --comments=false --config-file=etc/terser.json --output=$file $file');
 	saveContent(file, '#!/usr/bin/env node\n${getContent(file)}');
 	command('git update-index --chmod=+x $file');
-	if (!isWindows) command('chmod +x $file');
+	if (systemName() != "Windows") command('chmod +x $file');
 }
--- scripts/Doc.hx
+++ scripts/Doc.hx
@@ -1,6 +1,7 @@
+//! --class-path src
 import Sys.*;
 import Tools.removeDirectory;
-import haxe.Json;
+import card.Version.*;
 import sys.FileSystem.*;
 import sys.io.File.*;
 
@@ -8,13 +9,13 @@ import sys.io.File.*;
 function main() {
 	if (exists("docs")) removeDirectory("docs");
 
-	command("haxe --define doc-gen --no-output --xml var/api.xml build_neko.hxml");
+	command("haxe --define doc-gen --no-output --xml var/api.xml build.hxml");
 	command("lix", [
 		"run", "dox",
 		"--define", "description", "Business card of Cedric Belin, full stack developer.",
 		"--define", "source-path", "https://github.com/cedx/card/blob/main/src",
 		"--define", "themeColor", "0xffc105",
-		"--define", "version", Json.parse(getContent("haxelib.json")).version,
+		"--define", "version", packageVersion,
 		"--define", "website", "https://github.com/cedx/card",
 		"--input-path", "var",
 		"--output-path", "docs",
