99540bf2e5068490ce12719463367a5e0254035f
Cédric Belin <cedric@belin.io>
2021-12-20 21:33:35 +0100

    Updated the "Publish" task

--- scripts/Tools.hx
+++ scripts/Tools.hx
@@ -15,17 +15,6 @@ function cleanDirectory(directory: String) for (entry in readDirectory(directory
 	else deleteFile(path);
 }
 
-/** Creates a ZIP archive from the specified file system entities. **/
-function compress(sources: Array<String>, destination: String) {
-	final output = write(destination);
-	final writer = new Writer(output);
-
-	var entries: Array<Entry> = [];
-	for (source in sources) entries = entries.concat(isDirectory(source) ? compressDirectory(source) : [compressFile(source)]);
-	writer.write(entries.list());
-	output.close();
-}
-
 /** Recursively deletes the specified `directory`. **/
 function removeDirectory(directory: String) {
 	cleanDirectory(directory);
@@ -35,31 +24,3 @@ function removeDirectory(directory: String) {
 /** Replaces in the specified `file` the substring which the `pattern` matches with the given `replacement`. **/
 function replaceInFile(file: String, pattern: EReg, replacement: String)
 	saveContent(file, pattern.replace(getContent(file), replacement));
-
-/** Compresses the content of the specified `directory` in ZIP format. **/
-private function compressDirectory(directory: String) {
-	var entries: Array<Entry> = [];
-	for (entry in readDirectory(directory)) {
-		final path = join([directory, entry]);
-		entries = entries.concat(isDirectory(path) ? compressDirectory(path) : [compressFile(path)]);
-	}
-
-	return entries;
-}
-
-/** Compresses the specified `file` in ZIP format. **/
-private function compressFile(file: String) {
-	final bytes = getBytes(file);
-	final entry: Entry = {
-		compressed: false,
-		crc32: Crc32.make(bytes),
-		data: bytes,
-		dataSize: bytes.length,
-		fileName: file,
-		fileSize: bytes.length,
-		fileTime: stat(file).mtime
-	};
-
-	entry.compress(9);
-	return entry;
-}
