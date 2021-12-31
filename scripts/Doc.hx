54ba6808dbc2e1a1c89e7ba5d683226c1d18f55e
Cédric Belin <cedric@belin.io>
2021-12-31 19:48:57 +0100

    Updated the API reference

--- scripts/Doc.hx
+++ scripts/Doc.hx
@@ -12,14 +12,14 @@ function main() {
 	command("haxe --define doc-gen --no-output --xml var/api.xml build.hxml");
 	command("lix", [
 		"run", "dox",
-		"--define", "description", "Business card of Cedric Belin, full stack developer.",
+		"--define", "description", "Business card of Cédric Belin, full stack developer.",
 		"--define", "source-path", "https://github.com/cedx/card/blob/main/src",
 		"--define", "themeColor", "0xffc105",
 		"--define", "version", packageVersion,
 		"--define", "website", "https://github.com/cedx/card",
 		"--input-path", "var",
 		"--output-path", "docs",
-		"--title", "Cedric Belin's Card",
+		"--title", "Cédric Belin's Card",
 		"--toplevel-package", "card"
 	]);
 
