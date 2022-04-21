9cc4c35ac687d80f624e9a6d4395822d931174fe
Cédric Belin <cedric@belin.io>
2022-04-21 02:35:59 +0200

    Ported the build system to Gulp

--- scripts/Clean.hx
+++
@@ -1,7 +0,0 @@
-import sys.FileSystem;
-
-/** Runs the script. **/
-function main() {
-	if (FileSystem.exists("bin/belin_card.js")) FileSystem.deleteFile("bin/belin_card.js");
-	Tools.cleanDirectory("var");
-}
