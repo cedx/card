436623c12115423f94d4225d26d1bc8c407636ad
Cédric Belin <cedric@belin.io>
2022-09-02 09:50:22 +0200

    Dropped the dependency on Commander.js

--- bin/belin_card.js
+++ bin/belin_card.js
@@ -1,12 +1,31 @@
 #!/usr/bin/env node
 import console from "node:console";
-import {program} from "commander";
+import {parseArgs} from "node:util";
 import pkg from "../package.json" assert {type: "json"};
 import {getCard} from "../src/index.js";
 
+// The usage information.
+const usage = `
+Print the business card of Cédric Belin, full stack developer.
+
+Usage:
+  npx @cedx/card
+
+Options:
+  -h, --help     Display this help.
+  -v, --version  Output the version number.
+`;
+
 // Start the application.
-program.name("npx @cedx/card")
-	.description("Print the business card of Cédric Belin, full stack developer.")
-	.version(pkg.version, "-v, --version")
-	.action(() => console.log(getCard(1)))
-	.parse();
+try {
+	const {values} = parseArgs({options: {
+		help: {short: "h", type: "boolean"},
+		version: {short: "v", type: "boolean"}
+	}});
+
+	console.log(values.help ? usage.trim() : (values.version ? pkg.version : getCard(1)));
+}
+catch (error) {
+	console.error(error instanceof Error ? error.message : error);
+	process.exit(1);
+}
