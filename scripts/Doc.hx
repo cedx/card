f5ebb01f5b5cc5bdc9918ab81721599f96d9acd9
Cédric Belin <cedric@belin.io>
2023-02-15 17:58:25 +0100

    Updated the build system

--- scripts/Doc.hx
+++ scripts/Doc.hx
@@ -7,9 +7,8 @@ import sys.io.File;
 function main() {
 	if (FileSystem.exists("docs")) Tools.removeDirectory("docs");
 
-	Sys.command("haxe", ["--define", "doc-gen", "--no-output", "--xml", "var/api.xml", "build.hxml"]);
-	Sys.command("lix", [
-		"run", "dox",
+	Sys.command("haxe --define doc-gen --no-output --xml var/api.xml build.hxml");
+	Sys.command("lix", ["run", "dox",
 		"--define", "description", "Business card of Cedric Belin, full stack developer.",
 		"--define", "source-path", "https://github.com/cedx/card/blob/main/src",
 		"--define", "themeColor", "0x772953",
