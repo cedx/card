3d7405b71647349482908fbca3232dd859f583db
Cédric Belin <cedric@belin.io>
2024-01-26 10:08:53 +0100

    Code factorization

---
+++ src/cli/usage.ts
@@ -0,0 +1,10 @@
+export default `
+Print the business card of Cédric Belin, full stack developer.
+
+Usage:
+  npx @cedx/card
+
+Options:
+  -h, --help     Display this help.
+  -v, --version  Output the version number.
+`;
