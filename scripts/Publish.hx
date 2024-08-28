b3550ec4a6123164513b96f2c5f4ccc263c8bbfb
Cédric Belin <cedric@belin.io>
2024-08-28 10:07:21 +0200

    No need to store the JS script in repository

--- scripts/Publish.hx
+++ scripts/Publish.hx
@@ -3,6 +3,7 @@ import belin_card.Platform;
 
 /** Publishes the package. **/
 function main() {
+	Sys.command("lix Dist");
 	Sys.command("npm publish");
 	for (action in ["tag", "push origin"]) Sys.command('git $action v${Platform.packageVersion}');
 }
