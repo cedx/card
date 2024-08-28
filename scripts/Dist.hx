47757c727a17ac8f8f51e1c9b6aa8e6900bb0cd5
Cédric Belin <cedric@belin.io>
2024-08-28 10:10:08 +0200

    Update the "Dist" script

--- scripts/Dist.hx
+++ scripts/Dist.hx
@@ -3,5 +3,5 @@ function main() {
 	final file = "bin/belin_card.js";
 	for (script in ["Clean", "Build", "Version"]) Sys.command('lix $script');
 	Sys.command('npx esbuild --allow-overwrite --bundle --legal-comments=none --log-level=warning --minify --outfile=$file --platform=node $file');
-	Sys.command('git update-index --chmod=+x $file');
+	if (Sys.systemName() != "Windows") Sys.command('chmod +x $file');
 }
