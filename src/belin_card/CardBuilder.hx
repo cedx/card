b62e4d9c8e146bbcdff3b07d838fb20e5648cf7f
Cédric Belin <cedric@belin.io>
2024-08-29 12:26:16 +0200

    Code formatting

--- src/belin_card/CardBuilder.hx
+++ src/belin_card/CardBuilder.hx
@@ -19,38 +19,38 @@ abstract CardBuilder(StringBuf) from StringBuf {
 	public inline function new() this = new StringBuf();
 
 	/** Appends a centered line of text. **/
-	public function center(text: String): CardBuilder
+	public function center(text: String)
 		return line('${repeat(" ", Math.floor((width - getLength(text)) / 2))}$text');
 
 	/** Appends a bottom border. **/
-	public function footer(): CardBuilder
+	public function footer()
 		return addLine('<green>╰${repeat("─")}╯</>');
 
 	/** Appends a top border. **/
-	public function header(): CardBuilder
+	public function header()
 		return addLine('<green>╭${repeat("─")}╮</>');
 
 	/** Adds a labeled value. **/
-	public function label(label: String, value: String): CardBuilder
+	public function label(label: String, value: String)
 		return line('<light_white>$label:</> '.lpad(" ", 10 + indent.length + "<light_white></>".length) + value);
 
 	/** Appends a line of text. **/
-	public function line(text = ""): CardBuilder
+	public function line(text = "")
 		return addLine('<green>│</>$text${repeat(" ", width - getLength(text))}<green>│</>');
 
 	/** Appends a line of plain text. **/
-	function addLine(text: String): CardBuilder {
+	function addLine(text: String) {
 		this.add('$indent$text${Program.newLine}');
 		return abstract;
 	}
 
 	/** Gets the length in characters of the specified text, excluding formatting tags. **/
-	function getLength(text: String): Int {
+	function getLength(text: String) {
 		final innerText = ~/<[^<]*>/g.replace(text, "");
 		return #if neko Utf8.length(innerText) #else innerText.length #end;
 	}
 
 	/** Repeats the specified `character` a given number of `times`. **/
-	function repeat(character: String, times = width): String
+	function repeat(character: String, times = width)
 		return [for (_ in 0...times) character].join("");
 }
