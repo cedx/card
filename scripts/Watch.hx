967d90c3bf08982740380d805f796832160791c0
Cédric Belin <cedric@belin.io>
2024-10-27 01:34:05 +0200

    Update the "Watch" script

--- scripts/Watch.hx
+++ scripts/Watch.hx
@@ -8,12 +8,7 @@ using tink.CoreApi;
 /** Watches for file changes. **/
 function main() {
 	measureCommand("lix Build --debug");
-	Sys.command("node --enable-source-maps bin/belin_card.js");
-
-	GlobWatcher.watch("src/belin/card/**/*.hx", done -> {
-		measureCommand(done, "haxe --debug build.hxml");
-		Sys.command("node --enable-source-maps bin/belin_card.js");
-	});
+	GlobWatcher.watch("src/belin/card/**/*.hx", done -> measureCommand(done, "haxe --debug build.hxml"));
 }
 
 /** Formats the specified `duration` in seconds. **/
