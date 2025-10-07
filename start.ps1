37afec2bacee7628e24a27e3165689697be68629
Cédric Belin <cedx@outlook.com>
2025-10-07 11:03:37 +0200

    Add a launch script

---
+++ start.ps1
@@ -0,0 +1,8 @@
+#!/usr/bin/env pwsh
+Set-StrictMode -Version Latest
+$ErrorActionPreference = "Stop"
+$PSNativeCommandUseErrorActionPreference = $true
+
+$commandPath = Get-Item $PSCommandPath
+$scriptRoot = $commandPath.LinkType ? (Split-Path $commandPath.LinkTarget) : $PSScriptRoot
+node $scriptRoot/bin/Belin.Card.js @args
